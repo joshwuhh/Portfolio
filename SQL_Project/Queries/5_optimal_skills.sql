94a69db4246e6866608645f651742ecb5a3b84bf
Joshuah <jrodriguez9719@gmail.com>
2025-01-17 13:53:05 -0500

    pull

--- SQL_Project/Queries/5_optimal_skills.sql
+++ SQL_Project/Queries/5_optimal_skills.sql
@@ -45,7 +45,6 @@ FROM skills_demand
 INNER JOIN average_salary ON skills_demand.skill_id = average_salary.skill_id
 WHERE demand_count > 10
 ORDER BY 
-     avg_salary DESC,
     demand_count DESC
-LIMIT 25
+LIMIT 10
 
